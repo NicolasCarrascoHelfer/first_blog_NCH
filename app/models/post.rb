0fe660b8656c3161821efb3a278fea369e2b9896
NicolasCarrascoHelfer <u202116532@upc.edu.pe>
2024-01-03 23:14:14 -0500

    functional form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     validates :title, presence:true, length: {minimum:3, maximum:50}
-    validates :body, presence:true, length: {minimum:3, maximum:50}
+    validates :body, presence:true, length: {minimum:3, maximum:5000}
     belongs_to :user
     has_many :comments, dependent: :destroy
 
